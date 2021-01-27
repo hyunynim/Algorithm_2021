f0ccc4b4ff018a1579ab67c52bf49e6f24fa9652
Honghyun Ahn <32119804+hyunynim@users.noreply.github.com>
2021-01-27 11:46:56 +0900

    Update 1007_permutation.cpp

--- TOOLS_OJ/1007_permutation.cpp
+++ TOOLS_OJ/1007_permutation.cpp
@@ -1,29 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main() {
+int main(){
     int n; scanf("%d", &n);
-
+    
     vector<int> seq(n), p(n - 2);
-
+    
     p.push_back(1);
     p.push_back(1);
-
-    for (int i = 0; i < n; ++i)
+    
+    for(int i = 0; i<n; ++i)
         scanf("%d", &seq[i]);
-
-    do {
+    
+    do{
         int sum = 0;
         vector<int> ans;
-        for (int i = 0; i < n; ++i) {
-            if (p[i] == 1) {
-                sum += seq[i];
+        for(int i = 0; i<n; ++i){
+            if(p[i] == 1){
+                sum += seq[i];   
                 ans.push_back(seq[i]);
             }
         }
-        if (sum == 0) {
+        if(sum == 0){
             printf("%d %d", ans[0], ans[1]);
             return 0;
         }
-    } while (next_permutation(seq.begin(), seq.end()));
+    } while(next_permutation(p.begin(), p.end()));
     printf("-1");
 }
