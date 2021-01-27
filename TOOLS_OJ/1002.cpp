475b0d1a1ec674d716a6fe4b97e0a73f323b60ae
Honghyun Ahn <32119804+hyunynim@users.noreply.github.com>
2021-01-27 09:47:47 +0900

    Create 1002.cpp

---
+++ TOOLS_OJ/1002.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; scanf("%d", &n);
+    vector<int> seq(n);
+    for(int i  = 0; i<n; ++i)
+        scanf("%d", &seq[i]);
+    sort(seq.begin(), seq.end());
+    printf("%d", seq[n/2]);
+}
