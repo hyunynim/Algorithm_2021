af9abd491899271cb7edf02b2e027bfa8324c987
Honghyun Ahn <32119804+hyunynim@users.noreply.github.com>
2021-02-19 16:43:44 +0900

    Create 1697_1.cpp

---
+++ BOJ/1697_1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int chk[202020];
+int main() {
+	int n, k; scanf("%d %d", &n, &k);
+	queue<int> q;
+	q.push(n);
+	chk[n] = 1;
+	while (q.size()) {
+		auto cur = q.front(); q.pop();
+		int nx[] = { cur - 1, cur + 1, cur * 2 };
+		if (cur == k) {
+			printf("%d", chk[cur] - 1);
+			return 0;
+		}
+		for (int i = 0; i < 3; ++i) {
+			if (0 <= nx[i] && nx[i] < 202020 && chk[nx[i]] == 0) {
+				q.push(nx[i]);
+				chk[nx[i]] = chk[cur] + 1;
+			}
+		}
+	}
+}
